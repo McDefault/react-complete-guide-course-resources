66988c041412012cf05c4e9da0f3f4ca29f1113f
Max Ottenhof <max.ottenhof@gmail.com>
2025-05-14 20:56:12 +0200

    Reusable CoreConcept component

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -10,7 +11,7 @@ function Header() {
     const dynamic_description = reactDescriptions[genRandomInt(2)];
     return (
         <header>
-            <img src={reactImg} alt="Stylized atom" />
+            <img src={reactImg} alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
                 {dynamic_description} React concepts you will need for almost any app you are
@@ -20,15 +21,43 @@ function Header() {
     )
 }
 
+function CoreConcept({img, title, description}) {
+    return (
+        <li>
+            <img src={img} alt=""/>
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    )
+}
+
 function App() {
-  return (
-    <div>
-        <Header />
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <Header/>
+            <main>
+                <section id="core-concepts">
+                    <h2>Time to get started!</h2>
+                    <ul>
+                        <CoreConcept
+                            title="Components"
+                            description="The Core UI building block"
+                            img={componentsImg}
+                        />
+                        <CoreConcept
+                            title="Props"
+                        />
+                        <CoreConcept
+                            title="Props"
+                        />
+                        <CoreConcept
+                            title="Props"
+                        />
+                    </ul>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
