edd467c0015b09b5598f3d540d7dc13ef134fe16
Max Ottenhof <max.ottenhof@gmail.com>
2025-05-15 11:17:49 +0200

    Storing custom components in separate files.

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,35 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
 import {CORE_CONCEPTS} from './data.js';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-    const dynamic_description = reactDescriptions[genRandomInt(2)];
-    return (
-        <header>
-            <img src={reactImg} alt="Stylized atom"/>
-            <h1>React Essentials</h1>
-            <p>
-                {dynamic_description} React concepts you will need for almost any app you are
-                going to build!
-            </p>
-        </header>
-    )
-}
-
-function CoreConcept({image, title, description}) {
-    return (
-        <li>
-            <img src={image} alt=""/>
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </li>
-    )
-}
+import Header from './components/Header.jsx';
+import CoreConcept from "./components/CoreConcepts.jsx";
 
 function App() {
     return (
