16666efe278b417a398d2d360b7835befdb6e512
Max Ottenhof <max.ottenhof@gmail.com>
2025-06-14 16:25:08 +0200

    Core logic for answer shuffling.

--- code/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -6,7 +6,8 @@ export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
     //derived state - computed value
     const activeQuestionIndex = userAnswers.length;
-
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]; //create new array reference copy in memory from original reference
+    shuffledAnswers.sort(() => Math.random() - 0.5); //sort alters original reference
 
     function handleClickAnswer(answer) {
         // use implicit parameter (previousUserAnswers) from the set method to inject old data from the state handler and append new data to it
@@ -23,7 +24,7 @@ export default function Quiz() {
                     {QUESTIONS[activeQuestionIndex].text}
                 </h2>
                 <ul id={"answers"}>
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (<li key={answer} className={"answer"}>
+                    {shuffledAnswers.map((answer) => (<li key={answer} className={"answer"}>
                             <button onClick={() => handleClickAnswer(answer)}>{answer}</button>
                         </li>))}
                 </ul>
