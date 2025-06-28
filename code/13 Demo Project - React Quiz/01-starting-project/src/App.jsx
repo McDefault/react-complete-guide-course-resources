59354fb7c664e9ad6afd98589fe4515500976f11
Max Ottenhof <max.ottenhof@gmail.com>
2025-06-28 13:51:09 +0200

    Routing starting set up and paths

--- code/13 Demo Project - React Quiz/01-starting-project/src/App.jsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/App.jsx
@@ -1,12 +1,20 @@
 import Header from './components/Header.jsx';
 import Quiz from "./components/Quiz.jsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Dashboard from "./components/Dashboard.jsx";
+
+// Routing paths
+const router = createBrowserRouter([
+    {path: "/", element: <Dashboard/>},
+    {path: "/quiz", element: <Quiz/>},
+])
 
 function App() {
     return (
         <>
             <Header></Header>
             <main>
-                <Quiz></Quiz>
+                <RouterProvider router={router}/>
             </main>
         </>
     )
