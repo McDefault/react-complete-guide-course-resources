850bf2a65b1538b3c048f8b59ebc2fd9b48de967
Max Ottenhof <max.ottenhof@gmail.com>
2025-06-28 15:24:50 +0200

    Routing adding simple error page

--- code/13 Demo Project - React Quiz/01-starting-project/src/App.jsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/App.jsx
@@ -2,12 +2,14 @@ import Quiz from "./components/Quiz.jsx";
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Dashboard from "./components/Dashboard.jsx";
 import RouterRoot from "./components/RouterRoot.jsx";
+import Error from "./components/Error.jsx";
 
 // Routing paths
 const router = createBrowserRouter([
     {
         path: "/",
         element: <RouterRoot/>,
+        errorElement: <Error/>,
         children: [
             {path: "/", element: <Dashboard/>},
             {path: "/quiz", element: <Quiz/>},
