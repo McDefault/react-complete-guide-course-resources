fccb0c933313ebcdee403851b8eb7d547edf4c51
Max Ottenhof <max.ottenhof@gmail.com>
2025-07-05 17:34:30 +0200

    Implement auth signup validation and submit feedback

--- code/13 Demo Project - React Quiz/01-starting-project/src/components/auth/AuthForm.jsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/components/auth/AuthForm.jsx
@@ -1,14 +1,23 @@
-import {Form, Link, useSearchParams} from 'react-router-dom';
+import {Form, Link, useActionData, useNavigation, useSearchParams} from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
   const [searchParams ] = useSearchParams();
   const isLogin = searchParams.get('mode') === 'login';
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+  const actionData = useActionData();
 
   return (
     <>
       <Form method="post" className={classes.form}>
+        {actionData && actionData.errors && (<ul>
+          {Object.values(actionData.errors).map((error) => (
+              <li key={error}>{error}</li>
+          ))}
+        </ul>)}
+        {actionData && actionData.message && <p>{actionData.message}</p>}
         <h1>{isLogin ? 'Log in' : 'Create a new user'}</h1>
         <p>
           <label htmlFor="email">Email</label>
@@ -22,7 +31,7 @@ function AuthForm() {
           <Link to={`?mode=${isLogin ? 'signup' : 'login'}`} >
             {isLogin ? 'Create new user' : 'Login'}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>{isSubmitting ? 'Submitting...' : 'Save'}</button>
         </div>
       </Form>
     </>
