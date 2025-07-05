7e4df6ff8cc3b183bd439465cac30c9e295fa740
Max Ottenhof <max.ottenhof@gmail.com>
2025-07-05 14:58:55 +0200

    Fetcher implementation for newsletter

--- code/13 Demo Project - React Quiz/01-starting-project/src/components/NewsletterSignup.jsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/components/NewsletterSignup.jsx
@@ -1,17 +1,30 @@
 import classes from './NewsletterSignup.module.css';
-import {Form} from "react-router-dom";
+import {useFetcher} from "react-router-dom";
+import {useEffect} from "react";
 
 function NewsletterSignup() {
-  return (
-    <Form method="post" className={classes.newsletter}>
-      <input
-        type="email"
-        placeholder="Sign up for newsletter..."
-        aria-label="Sign up for newsletter"
-      />
-      <button>Sign up</button>
-    </Form>
-  );
+    const fetcher = useFetcher(); //trigger action/loader without navigation (behind the scenes)
+    const {data, state} = fetcher;
+
+    useEffect(() => {
+        if (state === 'idle' && data && data.message) {
+            window.alert(data.message);
+        }
+    }, [data, state]);
+
+    return (
+        <fetcher.Form
+            method="post"
+            action={"/newsletter"}
+            className={classes.newsletter}>
+            <input
+                type="email"
+                placeholder="Sign up for newsletter..."
+                aria-label="Sign up for newsletter"
+            />
+            <button>Sign up</button>
+        </fetcher.Form>
+    );
 }
 
 export default NewsletterSignup;
