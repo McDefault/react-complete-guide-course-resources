aaaaa05d2f9dc00cdab99ff0c0d69ba8bef1464d
M. Ottenhof <max.ottenhof@gmail.com>
2025-08-17 17:34:54 +0200

    TypeScript refactor imports for EventList component

--- code/13 Demo Project - React Quiz/01-starting-project/src/components/event/EventsList.tsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/components/event/EventsList.tsx
@@ -1,10 +1,11 @@
-import classes from './EventsList.module.css';
 import {Link} from "react-router-dom";
-import Event from "../../models/Event.ts";
+import {type FC} from "react";
+import type {Event} from "../../models/Event.ts";
+import classes from './EventForm.module.css';
 
 type EventsListProp = { events: Event[] };
 
-const EventsList: React.FC<EventsListProp> = ({events}) => {
+const EventsList: FC<EventsListProp> = ({events}) => {
     return (
         <div className={classes.events}>
             <h1>All Events</h1>
