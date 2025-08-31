71a41caa070b5fb23e01fbbf01fe90b440052692
M. Ottenhof <max.ottenhof@gmail.com>
2025-08-31 19:12:11 +0200

    Fix imports

--- code/13 Demo Project - React Quiz/01-starting-project/src/components/RouterRoot.tsx
+++ code/13 Demo Project - React Quiz/01-starting-project/src/components/RouterRoot.tsx
@@ -1,7 +1,5 @@
-import {Outlet, useLoaderData, useSubmit} from "react-router-dom";
+import {Outlet} from "react-router-dom";
 import Header from "./Header.tsx";
-import {useEffect} from "react";
-import {getTokenDuration} from "../util/auth.ts";
 import AuthContextProvider from "../store/auth-context.tsx";
 
 export default function RouterRoot() {
